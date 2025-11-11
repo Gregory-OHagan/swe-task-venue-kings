66711dec3c1c2e50fd1a6975e1a63fa840263396
Gregory O <gregoryrohagan@gmail.com>
2025-11-11 10:07:45 -0800

    Added additional endpoints, refactoring the request function to be more generic/avoid duplication of the retry and backoff code. Also added timeouts.

--- main.py
+++ main.py
@@ -13,65 +13,147 @@ async def main():
         config = json.load(infile)
     # TODO: consider adding defaults to loaded config in case of a malformed config file
 
-    # Process each endpoint
-    # TODO: implement at least 2 additional endpoints
-    (data_d, successful_calls, failed_calls, is_failure), = await asyncio.gather(get_data_source_d(config))
+    # Process each endpoint asynchronously
+    res_a, res_c, res_d = await asyncio.gather(get_data(config, request_source_a, "jsonplaceholder", "a."),
+                                               get_data(config, request_source_c, "escuelajs", "c."),
+                                               get_data(config, request_source_d, "dummyjson", "d."))
+    res_list = [res_a, res_c, res_d]
+
+    # Calculate combined statistics
+    successful_calls = sum([x[1] for x in res_list])
+    failed_calls = sum([x[2] for x in res_list])
+    success_rate = successful_calls / (successful_calls + failed_calls)
+
+    # Amalgamate the
+    data = []
+    for res in res_list:
+        data.extend(res[0])
+
+    # TODO: do something with reported errors
 
     # Calculate the processing time
     dt_end = datetime.datetime.now()
     duration = (dt_end - dt_start).total_seconds()
 
-    success_rate = successful_calls / (successful_calls + failed_calls)
-
     # Write the results to an output file
-    write_output(data_d, duration, success_rate, ["endpoint_d"])
+    write_output(data, duration, success_rate, ["a", "c", "d"])
 
 
-# Get data from the fourth provided sample source (https://dummyjson.com/products)
+# Fetches data from https://jsonplaceholder.typicode.com/posts, formatted as 20 item pages.
+# Note this pagination is intentionally inefficient; it is intended to mock data from a source that is paginated.
+# Parameters:
+# - session: an open aiohttp.ClientSession().
+# - page: the requested page number.
+# Returns:
+# - the status of the web request return
+# - the response json
+async def request_source_a(session, page):
+    async with session.get("https://jsonplaceholder.typicode.com/posts") as response:
+        data = await response.json() if response.status == 200 else []
+
+        # clip the data to simulate pagination.
+        start = 20 * (page - 1)
+        end = 20 * page
+        if start >= len(data):
+            return response.status, {'products': []}
+        if end < len(data):
+            data = data[:end]
+        return response.status, {'products': data[start:]}
+
+
+# Fetches data from https://api.escuelajs.co/api/v1/products, formatted as 25 item pages.
+# Note this pagination is intentionally inefficient; it is intended to mock data from a source that is paginated.
+# Parameters:
+# - session: an open aiohttp.ClientSession().
+# - page: the requested page number.
+# Returns:
+# - the status of the web request return
+# - the response json
+async def request_source_c(session, page):
+    async with session.get("https://api.escuelajs.co/api/v1/products") as response:
+        data = await response.json() if response.status == 200 else []
+        # clip the data to simulate pagination.
+        start = 25 * (page - 1)
+        end = 25 * page
+        if start >= len(data):
+            return response.status, {'products': []}
+        elif end < len(data):
+            data = data[:end]
+
+        # Format the 'category' value to match the expected format, discarding the extra data
+        # This is not part of mocking the data, and is used so that format-specific processing is done in the
+        # "request_source_<x>" function while all the generic/standardized processing can stay in get_data
+        for item in data:
+            item['category'] = item['category']['slug']
+        return response.status, {'products': data[start:]}
+
+
+# Fetches data from https://dummyjson.com/products, formatted as 20 item pages.
+# Parameters:
+# - session: an open aiohttp.ClientSession().
+# - page: the requested page number.
+# Returns:
+# - the status of the web request return
+# - the response json.
+async def request_source_d(session, page):
+    async with session.get("https://dummyjson.com/products?skip={}&limit=20".format((page - 1) * 20)) as response:
+        data = await response.json()
+        return response.status, data
+
+
+# Fetches and processes data. Also handles network retries.
 # Parameters:
 # - config: a dictionary with configuration variables, typically loaded from a json file
+# - request func: function of form (aiohttp.ClientSession(), page_num) => (response_status, response_json)
+#   - used for specifying the data source
+# - source_string: string. The text name of the data source
+# - source_prefix: string. The prefix used to create the unified_id field.
+#   - must be unique to guarantee unified_id field is unique.
 # Return: A list of processed objects (dictionaries), each corresponding to one product
-async def get_data_source_d(config):
+async def get_data(config, request_func, source_string, source_prefix):
     data = []
     success_count = 0
     fail_count = 0
     is_failure = False
 
-    async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=config["timeout_seconds"])) as session:
         is_more_data = True
-        skip = 0
+        page = 1
         retry_count = 0
         while is_more_data:
-            async with session.get("https://dummyjson.com/products?skip={}&limit=20".format(skip)) as response:
-                # Retry on request failure
-                if response.status != 200:
-                    fail_count += 1
-                    # End early if we fail too many times in a row
-                    if retry_count >= config["num_retries"]:
-                        is_failure = True
-                        break
-                    # Otherwise, retry with an exponential backoff
-                    else:
-                        delay = config["first_retry_delay_seconds"] \
-                                * pow(config["retry_backoff_multiplier"], retry_count)
-                        retry_count += 1
-                        await asyncio.sleep(delay)
-                        continue
+            try:
+                status, response_json = await request_func(session, page)
+            except asyncio.TimeoutError:
+                status = "timeout"
 
-                success_count += 1
-                retry_count = 1
-
-                response_json = await response.json()
-                if len(response_json['products']) > 0:
-                    res = process_one_set(response_json['products'], "jsonplaceholder.typicode.com", "d.")
-                    data.extend(res)
-
-                    skip += 20
-
-                    await asyncio.sleep(config['endpoint_delay_between_requests_seconds'])
-
+            # Retry on request failure
+            if status != 200:
+                fail_count += 1
+                # End early if we fail too many times in a row
+                if retry_count >= config["num_retries"]:
+                    is_failure = True
+                    break
+                # Otherwise, retry with an exponential backoff
                 else:
-                    is_more_data = False
+                    delay = config["first_retry_delay_seconds"] \
+                            * pow(config["retry_backoff_multiplier"], retry_count)
+                    retry_count += 1
+                    await asyncio.sleep(delay)
+                    continue
+
+            success_count += 1
+            retry_count = 1
+
+            if len(response_json['products']) > 0:
+                res = process_one_set(response_json['products'], source_string, source_prefix)
+                data.extend(res)
+
+                page += 1
+
+                await asyncio.sleep(config['endpoint_delay_between_requests_seconds'])
+
+            else:
+                is_more_data = False
 
     return data, success_count, fail_count, is_failure
 
@@ -88,8 +170,8 @@ def process_one_set(data, source, id_prefix):
             'id': id_prefix + str(product['id']),
             'title': product['title'],
             'source': source,
-            'price': product['price'],
-            'category': product['category'],
+            'price': product['price'] if 'price' in product else None,
+            'category': product['category'] if 'category' in product else None,
             'processed_at': datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
         }
         result.append(product_obj)
@@ -114,7 +196,7 @@ def write_output(data, processing_time, success_rate, sources):
     }
 
     with open("results.json", "w") as outfile:
-        json.dump(output, outfile)
+        json.dump(output, outfile, indent=4)
 
 
 # Run the script
--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
-This projects is for Gregory O'Hagan's application to Venue King.
+This project is for Gregory O'Hagan's application to Venue King.
+
+## Instructions
+
+Run the file "main.py" using Python.
 
 ## Requirements:
 
@@ -9,8 +13,28 @@ This projects is for Gregory O'Hagan's application to Venue King.
 ## Assumptions:
 
 Here are the assumptions made about the requirements when writing this script.
-If these assumptions are false, please let me know and I'll update this project to match
-* The web calls need to be handled efficiently in terms of memory,
+If any of these assumptions are incorrect, please let me know and I'll update this project to match
+* The web call responses need to be handled efficiently in terms of memory,
 but the contents of the output file do fit in memory.
 If this isn't true, then the output file would need to be written in chunks with guaranteed unique ids
 instead of amalgamating the results before writing the output file.
+Unless this is explicitly necessary, using json.dump is far better than writing the file by hand.
+If it was necessary, it would also require a rework of the request timing to avoid the case of requests
+being sent faster than they are processed, stacking up queued tasks in memory.
+* There are no duplicate items. If there were, duplicate items within one source would be easy
+to add without hurting runtime by changing the list of items to a dictionary of items.
+If there were duplicates between sources, this would additionally require the unified ids to
+be created in a way to match items across sources.
+* The requirements specified that web requests would return paginated data with 20 items.
+The request functions are mocked to provide this behaviour as the sample sources do not provide this,
+but the rest of the implementation would handle larger pages without issue (up to memory limits).
+* Object fields that are not present in the provided/mock data should be set to None/null.
+
+
+## Areas for future improvements
+
+If I was planning to maintain and reuse this project in the future (as opposed to using it as a one-off script),
+here are some areas I'd change to support this.
+
+* The endpoints are currently hard-coded. I would update this to allow selecting a sublist of implemented
+endpoints through a config file.
