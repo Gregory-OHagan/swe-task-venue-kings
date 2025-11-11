3eb9045abb8649cf3307e15590568fd260c83545
Gregory O <gregoryrohagan@gmail.com>
2025-11-11 01:49:13 -0800

    added retry with backoff to failed web requests

--- main.py
+++ main.py
@@ -15,14 +15,16 @@ async def main():
 
     # Process each endpoint
     # TODO: implement at least 2 additional endpoints
-    data_d = await asyncio.gather(get_data_source_d(config))
+    (data_d, successful_calls, failed_calls, is_failure), = await asyncio.gather(get_data_source_d(config))
 
     # Calculate the processing time
     dt_end = datetime.datetime.now()
     duration = (dt_end - dt_start).total_seconds()
 
+    success_rate = successful_calls / (successful_calls + failed_calls)
+
     # Write the results to an output file
-    write_output(data_d, duration, 0.97, ["endpoint_d"])
+    write_output(data_d, duration, success_rate, ["endpoint_d"])
 
 
 # Get data from the fourth provided sample source (https://dummyjson.com/products)
@@ -31,25 +33,47 @@ async def main():
 # Return: A list of processed objects (dictionaries), each corresponding to one product
 async def get_data_source_d(config):
     data = []
+    success_count = 0
+    fail_count = 0
+    is_failure = False
+
     async with aiohttp.ClientSession() as session:
         is_more_data = True
         skip = 0
+        retry_count = 0
         while is_more_data:
-            async with session.get('https://dummyjson.com/products?skip={}&limit=20'.format(skip)) as response:
-                # TODO: ensure web request is a success, implement retries/backoff
-                status = response.status
+            async with session.get("https://dummyjson.com/products?skip={}&limit=20".format(skip)) as response:
+                # Retry on request failure
+                if response.status != 200:
+                    fail_count += 1
+                    # End early if we fail too many times in a row
+                    if retry_count >= config["num_retries"]:
+                        failure = True
+                        break
+                    # Otherwise, retry with an exponential backoff
+                    else:
+                        delay = config["first_retry_delay_seconds"] \
+                                * pow(config["retry_backoff_multiplier"], retry_count)
+                        retry_count += 1
+                        await asyncio.sleep(delay)
+                        continue
+
+                success_count += 1
+                retry_count = 1
+
                 response_json = await response.json()
                 if len(response_json['products']) > 0:
                     res = process_one_set(response_json['products'], "jsonplaceholder.typicode.com", "d.")
                     data.extend(res)
 
                     skip += 20
+
                     await asyncio.sleep(config['endpoint_delay_between_requests_seconds'])
 
                 else:
                     is_more_data = False
 
-    return data
+    return data, success_count, fail_count, is_failure
 
 
 # Converts the json response into a list of objects
